27f8af5c9f795989894dd3d86a832c1cb71f1048
Fernando Torres <nandotorresbrasil@gmail.com>
2026-07-06 19:48:16 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.javanauta.agendadortarefas.infrastructure.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Inicializa o filtro customizado do JWT
+        JwtRequestFilter jwtRequestFilter = new JwtRequestFilter(jwtUtil, userDetailsService);
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desativa proteção CSRF para APIs REST
+                .authorizeHttpRequests(authorize -> authorize
+                        // 1. Endpoints Públicos (Acesso livre sem Token)
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Define a API como Stateless
+                )
+                // Injeta o filtro do JWT antes do filtro padrão de autenticação por usuário/senha
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
